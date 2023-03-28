5db04f7570230c18caee678e5019b89c979de13d
Murilo <mbalves1@outlook.com>
2023-03-28 19:25:57 -0300

    [feat] - init plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { myFirstPlugin } from './toolTipPlugin'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(myFirstPlugin, {options: true})
+  .mount('#app')
---
+++ src/toolTipPlugin/index.js
@@ -0,0 +1,13 @@
+// const myFirstPlugin = {
+  // install(app, options) {
+    //console.log("Hello world", options);
+  //}
+//}
+
+ function myFirstPlugin(app, options) {
+  console.log("Hello world", options);
+}
+
+export {
+  myFirstPlugin
+}
